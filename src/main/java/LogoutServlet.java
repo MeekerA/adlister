a6268f2e52d863d7343c4f297b528615bec521d3
Austin Meeker <austinmeeker@me.com>
2016-12-15 14:53:46 -0600

    first commit

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -1,14 +1,20 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
-public class LogoutServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+/**
+ * Created by Austin on 12/15/16.
+ */
+
+@WebServlet(name = "LogOutServlet", urlPatterns = "/logout")
+public class LogOutServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().removeAttribute("user");
         request.getSession().invalidate();
-        response.sendRedirect("/login");
+
+        request.getRequestDispatcher("/WEB-INF/logout.jsp").forward(request, response);
     }
 }
